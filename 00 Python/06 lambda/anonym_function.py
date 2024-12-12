db6f89ba7963e68ed15f15042185799423fabc3d
JS <souhrada@trebesin.cz>
2024-12-12 17:27:11 +0100

    +12.12 komentáře

--- 00 Python/06 lambda/anonym_function.py
+++ 00 Python/06 lambda/anonym_function.py
@@ -1,12 +1,21 @@
+# klasické funkce
 def hello():
     print("Hello")
 
+
 def plus10(x):
-    print(x+10)
+    print(x + 10)
 
 
-a = lambda x : x + 10
+# anonymní funkce, které vytváříme klíčovým slovem lambda
+# anonymní funkce můžeme uložit pod proměnnou nebo použít přímo (např. v tkinter)
+# lambda parametr : co provedeme s parametrem (automaticky returnují výsledek)
+a = lambda x: x + 10
+
+# anonymní funkce mohou být pouze na 1 řádek
+# anonymní funkce mohou být i bez parametru
 b = lambda: print("Hello")
+
 
 print(a(0))
 
