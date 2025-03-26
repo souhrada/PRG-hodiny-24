f8721fb2826783ba3bf366614710090253fbc36b
JS <souhrada@trebesin.cz>
2025-03-26 08:13:46 +0100

    +20.3.

--- procvičování/projekty/pygame/aktuální verze/player.py
+++ procvičování/projekty/pygame/aktuální verze/player.py
@@ -3,11 +3,12 @@ from utility import image_cutter
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
+        super().__init__()
         self.x = 150
         self.y = 150
         self.spritesheet = pygame.image.load("assets/characters/player/man_brownhair_run.png").convert_alpha()  
-        self.img = image_cutter(self.spritesheet, 0, 0, 15, 16, 3)
-        self.rect = self.img.get_rect(midbottom=(self.x, self.y))
+        self.image = image_cutter(self.spritesheet, 0, 0, 15, 16, 3)
+        self.rect = self.image.get_rect(midbottom=(self.x, self.y))
         self.index = 0
         self.speed = 10
         self.lives = 3
@@ -20,4 +21,24 @@ class Player(pygame.sprite.Sprite):
         if self.index >= frame_count:
             self.index = 0
         
-        self.img = image_cutter(self.spritesheet, int(self.index), direction, 15, 16, 3)
+        self.image = image_cutter(self.spritesheet, int(self.index), direction, 15, 16, 3)
+
+
+    def update(self):
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_w]:
+            self.animation(1)
+            self.rect.top -= self.speed
+        if key[pygame.K_a]:
+            self.animation(2)
+            self.rect.left -= self.speed
+        if key[pygame.K_s]:
+            self.animation(0)
+            self.rect.bottom += self.speed
+        if key[pygame.K_d]:
+            self.animation(3)
+            self.rect.right += self.speed
+    
+    def draw(self, screen):
+        screen.blit(self.img, self.rect)
