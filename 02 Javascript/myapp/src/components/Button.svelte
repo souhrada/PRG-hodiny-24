fa49891e6d287d66ee6736900f6aff3ecedae615
JS <souhrada@trebesin.cz>
2026-02-20 13:13:45 +0100

    +20.2 - svelte základy

---
+++ 02 Javascript/myapp/src/components/Button.svelte
@@ -0,0 +1,14 @@
+<button>Component button</button>
+
+<style>
+    button {
+        background-color: salmon;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 10000px;
+    }
+
+    button:hover {
+        background-color: rgb(245, 156, 146);
+    }
+</style>
