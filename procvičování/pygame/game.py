e93997a30c1aadcb5fecb241b559c1a071d4debd
JS <souhrada@trebesin.cz>
2026-04-08 10:47:36 +0200

    +8.4

--- procvičování/pygame/game.py
+++ procvičování/pygame/game.py
@@ -11,9 +11,16 @@ running = True
 player_x = 50
 player_y = 150
 
-player_rect = pygame.Rect(player_x, player_y, 100, 100)
-player_surface = pygame.Surface((player_rect.width, player_rect.height))
-player_surface.fill((0, 255, 0))
+# player_surface = pygame.Surface((player_rect.width, player_rect.height))
+# player_surface.fill((0, 255, 0))
+
+player_image = pygame.image.load("player.png").convert_alpha()
+player_rect = player_image.get_rect(center=(player_x, player_y))
+
+enemy_image = pygame.image.load("enemy.png").convert_alpha()
+enemy_rect = enemy_image.get_rect(center=(200, 200))
+
+enemy_speed = 5
 
 while running:
     # poll for events
@@ -35,10 +42,20 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
-    # pygame.draw.rect(screen, "blue", (player_x, player_y, 100, 100))
-    screen.blit(player_surface, player_rect)
 
     # RENDER YOUR GAME HERE
+
+    # pygame.draw.rect(screen, "blue", (player_x, player_y, 100, 100))
+    screen.blit(player_image, player_rect)
+    screen.blit(enemy_image, enemy_rect)
+
+    enemy_rect.y += enemy_speed
+
+    if enemy_rect.bottom > screen.get_height() or enemy_rect.top < 0:
+        enemy_speed *= -1
+
+    if player_rect.colliderect(enemy_rect):
+        print("Auuu!")
 
     # flip() the display to put your work on screen
     pygame.display.flip()
